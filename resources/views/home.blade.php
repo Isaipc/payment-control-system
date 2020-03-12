63953fd99959faeb2a1a77bca1c3ea05f52cb90d
Isai Polito <ispolitoc@gmail.com>
2020-03-11 22:27:49 -0600

    Cambiar las etiquetas y mensajes a español

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Panel de control</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,15 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    @guest
+                    @else
+                    <h2 class="mx-auto text-center">
+                        Bienvenido
+                        <strong class="text-uppercase">
+                            {{ Auth::user()->name }}
+                        </strong>
+                    </h2>
+                    @endguest
                 </div>
             </div>
         </div>
